8b5c7665cc2a4862a7eb7054c974c45d01212d5a
Jianmu Deng <deng5945@uwlax.edu>
2019-06-16 00:48:37 +0800

    chapter 10 sliver challenge

---
+++ coffeerun/scripts/formhandler.js
@@ -0,0 +1,55 @@
+(function (window){
+  'use strict';
+  var App = window.App || {};
+
+  function FormHandler(selector){
+    if(!selector){
+      throw new Error('No selector provided');
+    }
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+
+    // update strengthOutput when slider changes and add color
+    var slider = document.getElementById("strengthLevel");
+    var sliderOutput = document.getElementById("strengthOutput");
+    var sliderLabel = document.getElementById('strengthLabel');
+
+    slider.addEventListener("input", function() {
+      sliderOutput.value = slider.value
+      var intensity = slider.value;
+      var intensityClass;
+      if (intensity < 34) {
+        intensityClass = "low";
+      }
+      else if (intensity < 68) {
+        intensityClass = "medium";
+      }
+      else {
+        intensityClass = "high";
+      }
+      sliderOutput.classList.remove('low', 'medium', 'high');
+      sliderOutput.classList.add(intensityClass);
+    });
+
+  };
+  FormHandler.prototype.addSubmitHandler = function(fn){
+    console.log('Setting submit handler for form');
+    this.$formElement.on('submit',function(event){
+      event.preventDefault();
+      var data = {};
+      $(this).serializeArray().forEach(function(item){
+        data[item.name] = item.value;
+        console.log(item.name + ' is ' + item.value);
+      });
+      console.log(data);
+      fn(data);
+      this.reset();
+      this.elements[0].focus();
+    });
+  };
+  App.FormHandler = FormHandler;
+  window.App = App;
+  var $ = window.jQuery;
+})(window);
--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,8 +1,13 @@
 (function (window){
   'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var FormHandler = App.FormHandler;
+  var formHandler = new FormHandler(FORM_SELECTOR);
   var myTruck = new Truck('ncc-1701', new DataStore());
+  formHandler.addSubmitHandler(myTruck.createOne.bind(myTruck));
+  console.log(formHandler);
   window.myTruck = myTruck;
 })(window);
