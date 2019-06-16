9dad10533e8ff2db03b2eb1c3930a94d930a8885
Jianmu Deng <deng5945@uwlax.edu>
2019-06-16 18:19:02 +0800

    chapter 11 done without golden challenge

---
+++ coffeerun/scripts/checklist.js
@@ -0,0 +1,79 @@
+(function (window){
+  'use strict';
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function CheckList(selector){
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+    this.$element = $(selector);
+    if (this.$element.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+
+
+  };
+
+  CheckList.prototype.addClickHandler = function(fn){
+    this.$element.on('click','input',function(event){
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
+
+  CheckList.prototype.addRow = function (coffeeOrder) {
+    this.removeRow(coffeeOrder.emailAddress);
+    var rowElement = new Row(coffeeOrder);
+    this.$element.append(rowElement.$element);
+  };
+
+  CheckList.prototype.removeRow = function(email){
+    this.$element
+    .find('[value="' + email + '"]')
+    .closest('[data-coffee-order="checkbox"]')
+    .remove();
+  };
+  function Row(coffeeOrder) {
+    var $div = $('<div></div>', {
+      'data-coffee-order': 'checkbox',
+      'class': 'checkbox',
+      'id':'pending'
+    });
+
+    var $label = $('<label></label>');
+
+    var $checkbox = $('<input></input>', {
+      type: 'checkbox',
+      value: coffeeOrder.emailAddress
+    });
+
+    var description = ' [' + coffeeOrder.strength + 'x]' + ' ';
+    if (coffeeOrder.flavor) {
+      description += coffeeOrder.flavor + ' ';
+    }
+    description += coffeeOrder.coffee + ', ';
+    description += ' (' + coffeeOrder.emailAddress + ')';
+    description += coffeeOrder.size;
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+
+    this.$element = $div;
+    var bgc = "";
+    switch (coffeeOrder.flavor) {
+      case 'caramel': bgc="blue";break;
+      case 'almond': bgc="green";break;
+      case'mocha': bgc="purple";break;
+    }
+    if (bgc) {
+      $label.css('background',bgc);
+    }
+  };
+
+
+  App.CheckList = CheckList;
+  window.App = App;
+})(window);
--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,13 +1,21 @@
 (function (window){
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
   var FormHandler = App.FormHandler;
-  var formHandler = new FormHandler(FORM_SELECTOR);
+  var CheckList = App.CheckList;
   var myTruck = new Truck('ncc-1701', new DataStore());
-  formHandler.addSubmitHandler(myTruck.createOne.bind(myTruck));
-  console.log(formHandler);
   window.myTruck = myTruck;
+
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+  var formHandler = new FormHandler(FORM_SELECTOR);
+  formHandler.addSubmitHandler(function(data){
+    myTruck.createOne.call(myTruck,data);
+    checkList.addRow.call(checkList,data);
+  });
+  // console.log(formHandler);
 })(window);
